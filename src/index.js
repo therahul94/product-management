335b087aad1c60318214f406f6b79d6dbdab6fbb
therahul94 <therahulkurmalkar@gmail.com>
2022-05-24 12:08:12 +0530

    1stPhase

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const express=require('express')
 const bodyParser=require('body-parser')
 const mongoose=require('mongoose')
 const route= require('./routes/route')
+const multer = require('multer')
 const app=express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
+app.use(multer().any())
 
 mongoose.connect("mongodb+srv://Avi9984:edmXH1jaB63lQyo8@cluster0.qfc4n.mongodb.net/group25",{
     useNewUrlParser:true
@@ -16,5 +18,5 @@ mongoose.connect("mongodb+srv://Avi9984:edmXH1jaB63lQyo8@cluster0.qfc4n.mongodb.
 app.use("/",route)
 
 app.listen(process.env.PORT || 3000,function(){
-    console.log('Express app running on the port' +(process.env.PORT || 3000))
+    console.log('Express app running on the port ' +(process.env.PORT || 3000))
 })
